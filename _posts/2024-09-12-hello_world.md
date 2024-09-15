1485c7c50e095779bfe5dcf0581b7602c72f1aab
Chen.S <chensunmac@gmail.com>
2024-09-15 01:56:56 -0400

    tags

---
+++ _posts/2024-09-12-hello_world.md
@@ -0,0 +1,7 @@
+---
+title: commit 1
+layout: single
+excerpt: "1st commit on the post for my website"
+tags:
+  - nan
+---
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "commit 1",
+        "excerpt":" ","categories": [],
+        "tags": ["nan"],
+        "url": "/hello_world/",
+        "teaser": null
+      }]
